08fec88036bb0327b7c8e15efa5b81aceb69c7aa
Elijah Silang <iv.elijah.s@gmail.com>
2020-12-30 12:30:39 -0600

    Initial React Code

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 30, fontWeight: 'bold'}}>Profile</Text>
+      <Image source={require('./assets/Bad-acting-headshot.png')} style={{height:350, marginBottom:10}}resizeMode='contain'></Image>
+      <Text style={{fontSize: 17}}>Name: Elijah Silang</Text>
+      <Text style={{fontSize: 17}}>Address: LBK, TX</Text>
+      <Text style={{fontSize: 17}}>Education Background: BS in CS</Text>
       <StatusBar style="auto" />
     </View>
   );
